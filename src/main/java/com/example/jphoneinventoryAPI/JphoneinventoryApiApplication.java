3f569ee21261a20449b486159db201150867439b
Khokhar Atif (UK) <khokhaa1@masst303308.local>
2018-11-16 09:49:39 +0000

    first commit for Java API project

---
+++ src/main/java/com/example/jphoneinventoryAPI/JphoneinventoryApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.jphoneinventoryAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JphoneinventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JphoneinventoryApiApplication.class, args);
+	}
+}
